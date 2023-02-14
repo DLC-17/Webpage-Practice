d2049a116d731036812e12c61349bf0b7c89ff57
David Coleman <dlc17@stmarys-ca.edu>
2023-02-14 10:38:33 -0800

    This takes a given string and incorporates it into the next written output

--- CS-192 first project/Program.cs
+++ CS-192 first project/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("I am the leader of this new world!");
+Console.WriteLine("Tell what is your name soldier?");
+string testString;
+Console.Write(":");
+testString = Console.ReadLine();
+Console.WriteLine($"Ah your name is, {testString} ,that is a fine name for a warrior!");
